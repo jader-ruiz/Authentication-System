d42ad5048314d32964e13f2f11e9880eee23ea70
Jader Ruiz <ruizjader.2707@gmail.com>
2026-08-10 08:24:22 -0500

    Create the dashboard window

---
+++ gui/dashboard.py
@@ -0,0 +1,78 @@
+import customtkinter as ctk
+
+class Dashboard:
+
+    def __init__(self,login):
+        self.login = login
+        self.dashboard = ctk.CTkToplevel(login.root)
+        self.interface()
+        self.frames()
+        self.c_button()
+        
+
+    def interface(self):
+        self.dashboard.geometry("750x600")
+        self.dashboard.title("Dashboard")
+        self.dashboard.resizable(False, False)
+
+    def frames(self):
+        self.header_frame = ctk.CTkFrame(master=self.dashboard)
+        self.header_frame.pack(fill="x")
+        
+        self.main_frame = ctk.CTkFrame(master=self.dashboard)
+        self.main_frame.pack(fill="both",expand=True)
+        
+        self.dashboard_frame = ctk.CTkFrame(master=self.main_frame,fg_color="transparent")
+        self.dashboard_frame.pack(pady=(20,0))
+        
+        self.bottom_frame = ctk.CTkFrame(master=self.dashboard)
+        self.bottom_frame.pack(fill="x")
+
+    def widgets(self):
+        self.c_labels()
+        self.c_button()
+
+    def c_labels(self):
+        self.font_main_title = ctk.CTkFont(family='Arial',size=20,weight="bold")
+        self.main_title = ctk.CTkLabel(master=self.header_frame,text="DASHBOARD",font=self.font_main_title)
+        self.main_title.pack(pady=10)
+
+        self.font_label = ctk.CTkFont(family='Arial',size=12,weight="bold")
+
+        self.s_title = ctk.CTkLabel(master=self.dashboard_frame,text="Welcome!",font=self.font_label)
+        self.s_title.grid(column=0,row=0,pady=(5,10))
+
+        self.username_label = ctk.CTkLabel(master=self.dashboard_frame,text="Username",font=self.font_label)
+        self.username_label.grid(row=1,column=0,pady=(0,5))
+        
+        self.email_label = ctk.CTkLabel(master=self.dashboard_frame,text="Email",font=self.font_label)
+        self.email_label.grid(row=2,column=0,pady=(0,5))
+
+        self.r_label = ctk.CTkLabel(master=self.dashboard_frame,text="Role",font=self.font_label)
+        self.r_label.grid(row=3,column=0,pady=(0,25))
+
+    def c_button(self):
+        self.edit_profile = ctk.CTkButton(
+            master=self.dashboard_frame,
+            text="Edit Profile"
+        )
+        self.edit_profile.grid(column=0,row=4,pady=(0,10))
+
+        self.change_password = ctk.CTkButton(
+            master=self.dashboard_frame,
+            text="Change Password"
+        )
+        self.edit_profile.grid(column=0,row=5,pady=(0,10))
+
+        self.delete_account = ctk.CTkButton(
+            master=self.dashboard_frame,
+            text="Delete Account"
+        )
+        self.edit_profile.grid(column=0,row=6)
+
+        self.logout = ctk.CTkButton(
+            master=self.bottom_frame,
+            text="Logout"
+        )
+        self.logout.pack()
+        
